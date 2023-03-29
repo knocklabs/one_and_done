ebe3f82523732e6f35cb100c4cd48dc74bc527f2
Brent Anderson <brent@knock.app>
2023-03-29 16:12:36 -0500

    v0.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+## 0.1.1
+
+* Fix bug in `OneAndDone.Plug` where `x-request-id` was being overwritten.
+* Introduce `x-original-request-id` header to preserve the original request ID.
+
 ## 0.1.0
 
 * Initial release
--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule OneAndDone.MixProject do
     [
       name: "One and Done",
       app: :one_and_done,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       package: package(),
